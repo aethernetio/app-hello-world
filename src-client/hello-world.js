4622180e72a58a66417310e7acb4de19f65d2cd0
Tony Richards <linterra@gmail.com>
2016-04-27 20:48:41 -0400

    PLaying around with value converters

--- src-client/hello-world.js
+++ src-client/hello-world.js
@@ -1,24 +1,4 @@
 export class HelloWorld {
   constructor() {
-    this.message = "Hello, World!";
-    this.attachedCount = 0;
-    console.log("Created new HelloWorld");
-  }
-
-  activate(params, routerConfig) {
-    console.log('Params: ' + String(params));
-    console.log(params);
-    console.log('Config: ' + String(routerConfig));
-    console.log(routerConfig);
-    this.params = params.params;
-    this.routerConfig = routerConfig;
-  }
-
-  attached() {
-    this.attachedCount += 1;
-  }
-
-  detached() {
-    this.attachedCount -= 1;
   }
 }
